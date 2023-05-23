d260adbebd028bf4255a340d1d8f2ffd77b63e70
Hiệp Hoàng <hoanghiep20179706@gmail.com>
2023-05-23 15:42:26 +0700

    Init projects

---
+++ src/main/java/com/lumera/wordsearch/WordSearchApplication.java
@@ -0,0 +1,13 @@
+package com.lumera.wordsearch;
+
+import static com.lumera.wordsearch.WordlistKt.readWordList;
+
+/**
+ * Entry point of application.
+ */
+public class WordSearchApplication {
+    public static void main(String[] args) {
+        var words = readWordList("wordlist.txt");
+        // ToDo: implement more
+    }
+}
