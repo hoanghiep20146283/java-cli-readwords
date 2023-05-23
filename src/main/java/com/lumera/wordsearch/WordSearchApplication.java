906c7ab1cd6da2e42c173ce321fc9b1b43134258
Hiệp Hoàng <hoanghiep20179706@gmail.com>
2023-05-23 15:51:34 +0700

    Add search command

--- src/main/java/com/lumera/wordsearch/WordSearchApplication.java
+++ src/main/java/com/lumera/wordsearch/WordSearchApplication.java
@@ -14,11 +14,11 @@ public class WordSearchApplication {
 //        // ToDo: implement more
 //    }
 
-    public static void main(String[] args) {
-        CommandLine cmd = new CommandLine(new SearchCommand());
+  public static void main(String[] args) {
+    CommandLine cmd = new CommandLine(new SearchCommand());
 //    cmd.setCaseInsensitiveEnumValuesAllowed(true)
 //        .setOut(myOutWriter()) .setErr(myErrWriter())  .setColorScheme(myColorScheme());
-        int exitCode = cmd.execute(args);
-        System.exit(exitCode);
-    }
+    int exitCode = cmd.execute(args);
+    System.exit(exitCode);
+  }
 }
