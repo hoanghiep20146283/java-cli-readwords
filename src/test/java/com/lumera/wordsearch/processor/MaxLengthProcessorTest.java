0b0f1be0fa6e136d075e8acb3522f5c05eeab133
Hiệp Hoàng <hoanghiep20179706@gmail.com>
2023-05-25 20:41:26 +0700

    Add unit tests

--- src/test/java/com/lumera/wordsearch/processor/MaxLengthProcessorTest.java
+++ src/test/java/com/lumera/wordsearch/processor/MaxLengthProcessorTest.java
@@ -1,7 +1,55 @@
 package com.lumera.wordsearch.processor;
 
+import java.util.List;
 import junit.framework.TestCase;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
 
 public class MaxLengthProcessorTest extends TestCase {
 
+  private MaxLengthProcessor processor;
+
+  @BeforeEach
+  public void setup() {
+    // Initialize the MaxLengthProcessor object for each test case
+    processor = new MaxLengthProcessor();
+  }
+
+  @Test
+  public void testSearch_WordWithLengthLessThanOrEqualOptionValue_ReturnsListWithWord() {
+    // Prepare the data
+    processor.setOptionValue(5L);
+
+    // Call the method under test
+    final List<String> result = processor.search("hello");
+
+    // Check the result
+    Assertions.assertEquals(1, result.size());
+    Assertions.assertEquals("hello", result.get(0));
+  }
+
+  @Test
+  public void testSearch_WordWithLengthGreaterThanOptionValue_ReturnsEmptyList() {
+    // Prepare the data
+    processor.setOptionValue(5L);
+
+    // Call the method under test
+    final List<String> result = processor.search("hello world");
+
+    // Check the result
+    Assertions.assertTrue(result.isEmpty());
+  }
+
+  @Test
+  public void testSearch_NullWord_ReturnsEmptyList() {
+    // Prepare the data
+    processor.setOptionValue(5L);
+
+    // Call the method under test
+    final List<String> result = processor.search(null);
+
+    // Check the result
+    Assertions.assertTrue(result.isEmpty());
+  }
 }
