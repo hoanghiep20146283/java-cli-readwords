82e18249ce641ccc7cef7a29caab30a616f851b1
Hiệp Hoàng <hoanghiep20179706@gmail.com>
2023-05-25 23:14:44 +0700

    Last commit

--- src/main/java/com/lumera/wordsearch/exception/ProcessorNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.lumera.wordsearch.exception;
-
-public class ProcessorNotFoundException extends RuntimeException {
-
-  public ProcessorNotFoundException(Throwable cause) {
-    super(cause);
-  }
-}
