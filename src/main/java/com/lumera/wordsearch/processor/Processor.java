a2250c1da73a8e4f00f6de38a546ab8c67c95e18
Hiệp Hoàng <hoanghiep20179706@gmail.com>
2023-05-25 22:30:23 +0700

    Clean code

--- src/main/java/com/lumera/wordsearch/processor/Processor.java
+++ src/main/java/com/lumera/wordsearch/processor/Processor.java
@@ -2,13 +2,35 @@ package com.lumera.wordsearch.processor;
 
 import java.util.List;
 
+/**
+ * This abstract class is the superclass of all processors has the role of searching for keywords
+ * that satisfy a certain condition (arguments input from cli, words input from wordlist.txt file)
+ *
+ * @author James Hoang
+ * @see ContainsOnlyProcessor
+ * @see ClasifyProcessor
+ * @see EndsWithProcessor
+ * @see StartsWithProcessor
+ * @see MaxLengthProcessor
+ * @see MinLengthProcessor
+ */
 public abstract class Processor<T> {
 
   protected T optionValue;
 
+  /**
+   * Set conditions for keywords to be processed by processor
+   *
+   * @param optionValue the argument input from CLI
+   */
   public void setOptionValue(T optionValue) {
     this.optionValue = optionValue;
   }
 
+  /**
+   * Takes a string and returns strings that satisfy the processor's condition
+   *
+   * @param word the word input from wordlist.txt
+   */
   public abstract List<String> search(String word);
 }
