fb91b92ab0ed8fef38e37f6ddfebda227c735472
Hiệp Hoàng <hoanghiep20179706@gmail.com>
2023-05-28 21:44:03 +0700

    Add and combine processor

--- src/main/java/com/lumera/wordsearch/processor/CombineProcessor.java
+++ src/main/java/com/lumera/wordsearch/processor/CombineProcessor.java
@@ -1,13 +1,7 @@
 package com.lumera.wordsearch.processor;
 
 import com.lumera.wordsearch.config.XmlConfig.CmdOptionConfig;
-import com.lumera.wordsearch.exception.ProcessorException;
-import com.lumera.wordsearch.service.ProcessorHelper;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 /**
  * This abstract class is the superclass of all associative processors whose role is to combine
@@ -22,33 +16,9 @@ import java.util.Set;
  * @see MinLengthProcessor
  */
 
-public abstract class CombineProcessor {
+@FunctionalInterface
+public interface CombineProcessor {
 
-  abstract void combineMatchedWords(Set<String> matchedWords, Set<String> matchindWords);
+  List<String> combine(String word, List<CmdOptionConfig> matchedCmdOptionConfigs);
 
-  public List<String> combine(String word, List<CmdOptionConfig> matchedCmdOptionConfigs) {
-    try {
-      if (matchedCmdOptionConfigs.isEmpty()) {
-        return Collections.singletonList(word);
-      }
-
-      final Set<String> matchedWords = new HashSet<>(
-          ProcessorHelper.processorTypeMap.get(matchedCmdOptionConfigs.get(0).getProcessorType())
-              .search(word));
-
-      for (int i = 1; i < matchedCmdOptionConfigs.size(); i++) {
-        final Set<String> matchindWords = new HashSet<>();
-        for (String matchedWord : matchedWords) {
-          matchindWords.addAll(
-              ProcessorHelper.processorTypeMap.get(
-                      matchedCmdOptionConfigs.get(i).getProcessorType())
-                  .search(matchedWord));
-        }
-        combineMatchedWords(matchedWords, matchindWords);
-      }
-      return new ArrayList<>(matchedWords);
-    } catch (Exception ex) {
-      throw new ProcessorException("Error Processing word: " + word, ex);
-    }
-  }
 }
--- src/main/java/com/lumera/wordsearch/processor/OrCombineProcessor.java
+++ src/main/java/com/lumera/wordsearch/processor/OrCombineProcessor.java
@@ -1,12 +1,37 @@
 package com.lumera.wordsearch.processor;
 
+import com.lumera.wordsearch.config.XmlConfig.CmdOptionConfig;
+import com.lumera.wordsearch.exception.ProcessorException;
+import com.lumera.wordsearch.service.ProcessorHelper;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
-public class OrCombineProcessor extends CombineProcessor {
+public class OrCombineProcessor implements CombineProcessor {
 
 
   @Override
-  void combineMatchedWords(Set<String> matchedWords, Set<String> matchindWords) {
-    matchedWords.addAll(matchindWords);
+  public List<String> combine(String word, List<CmdOptionConfig> matchedCmdOptionConfigs) {
+    try {
+      if (matchedCmdOptionConfigs.isEmpty()) {
+        return Collections.singletonList(word);
+      }
+
+      final Set<String> matchedWords = new HashSet<>(
+          ProcessorHelper.processorTypeMap.get(matchedCmdOptionConfigs.get(0).getProcessorType())
+              .search(word));
+
+      for (int i = 1; i < matchedCmdOptionConfigs.size(); i++) {
+        matchedWords.addAll(
+            ProcessorHelper.processorTypeMap.get(
+                    matchedCmdOptionConfigs.get(i).getProcessorType())
+                .search(word));
+      }
+      return new ArrayList<>(matchedWords);
+    } catch (Exception ex) {
+      throw new ProcessorException("Error Processing word: " + word, ex);
+    }
   }
 }
